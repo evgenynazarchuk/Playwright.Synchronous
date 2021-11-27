fe75e9e7007f59b9930cb1dc4ca3cd3ce1080c63
Evgeny Nazarchuk <68129901+evgenynazarchuk@users.noreply.github.com>
2021-11-27 21:31:40 +0300

    add playwright create

---
+++ Playwright.Synchronous/PlaywrightSynchronous.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Playwright;
+
+namespace Playwright.Synchronous;
+
+public static class PlaywrightSynchronous
+{
+    public static IPlaywright Create()
+    {
+        return Microsoft.Playwright.Playwright.CreateAsync().GetAwaiter().GetResult();
+    }
+}
